997f98ad1a0b35a62702b95fdde3d873b55976b9
Itay.Talias <itay.talias@gmail.com>
2022-11-30 14:28:52 +0200

    .

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from config import token
-from Github.github import get_files_from_organization
+from Github.github import get_files_from_organization,get_context_from_file
 
 
 
-f = get_files_from_organization(token,"Beavers-linter")
-print(f)
+get_files_from_organization(token,"Beavers-linter")
