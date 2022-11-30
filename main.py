0c1a1f1d0e264c85d149d17449c0c981ecf4eaed
Adi-Shuker <adielgali57@gmail.com>
2022-11-30 14:26:00 +0200

    bcrypt

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from config import token
-from Github.github import print_all_files_in_organization
+from Github.github import print_all_files_in_organization, get_files_from_repo,connect_to_github,get_repos, get_context_from_file
 
-print_all_files_in_organization(token,"Beavers-linter")
+organization = connect_to_github(token, "Beavers-linter")
+repos = get_repos(organization)
+for repo in repos:
+    files = get_files_from_repo(repo)
+    for file in files:
+        print("file:", file)
+        context = get_context_from_file(file)
+        context.split('\n')
+        print("context:", context)
