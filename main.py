fcfa1253df98657777cc1151c461de478d9f3113
Adi-Shuker <adielgali57@gmail.com>
2022-12-01 10:01:20 +0200

    remove github_model file

--- main.py
+++ main.py
@@ -4,6 +4,4 @@ from data_analyze.analyze_engine import analyze_file
 
 files = get_files_from_organization(TOKEN, "Beavers-linter")
 for file in files:
-    # print("file name: ", file)
-    # print("file content: ", get_context_from_file(file["file"]))
     analyze_file(file["file"], "hashlib")
