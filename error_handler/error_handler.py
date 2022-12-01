2d7400dbff1803046f266398ce09a79f9b51b195
shalev <shalevsharf@gmail.com>
2022-12-01 15:15:49 +0200

    fast api refactoring

---
+++ error_handler/error_handler.py
@@ -0,0 +1,11 @@
+BAD_REQUEST_MSG = "General error pls refresh the page"
+TOKEN_ERROR_MSG = "Invalid transaction operation"
+ORGANIZATION_NAME = "Invalid organization name"
+
+def post_request(client_data: dict):
+    if client_data == None:
+        raise ValueError(BAD_REQUEST_MSG)
+    if (not isinstance(client_data.token, (str))):
+        raise TypeError(TOKEN_ERROR_MSG)
+    if (not isinstance(client_data.organization, (str))):
+        raise TypeError(ORGANIZATION_NAME)
--- server.py
+++ server.py
@@ -1,21 +1,22 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Request
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from typing import List
 from vcs_api.github_api import get_files_from_organization
+from error_handler import error_handler
 
 
 app = FastAPI()
-#, status_code = status.HTTP_200_OK
-@app.get("/files")
-def get_files() -> List[dict]:
-    pass
-    # try:
-    #     transactions = db_manger.get_all_transactions()
-    # except pymysql.MySQLError as error:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=error)
-    # return transactions
+
+@app.post("/files", status_code=status.HTTP_201_CREATED)
+async def get_files(request: Request) -> List[dict]:
+    try:
+        result: dict = await request.json()
+        error_handler.post_request(client_data = result)
+        files = get_files_from_organization(result.token,result.organization)
+    except Exception as error:
+        raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR, detail=error)
+    return files
 
 
 origins = [
