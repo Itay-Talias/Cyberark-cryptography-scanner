3564d639422694a202769d094d05bd12a2a92ec7
Adi-Shuker <adielgali57@gmail.com>
2022-12-01 10:32:52 +0200

    Concentration of all data in one data structure

---
+++ data_analyze/libraries/libraries_data.py
@@ -0,0 +1,29 @@
+libraries = {
+    "python": {
+        "hashlib": {
+            "category": "hash",
+            "words": ["sha1",
+                      "sha224",
+                      "sha256",
+                      "sha384",
+                      "sha512",
+                      "blake2b",
+                      "blake2s",
+                      "md5",
+                      "sha3_224",
+                      "sha3_256",
+                      "sha3_384",
+                      "sha3_512",
+                      "shake_128",
+                      "shake_256",
+                      "scrypt"]
+        },
+        "bcrypt": {
+            "category": "hash",
+            "words": ["hashpw",
+                      "kdf"]
+        }
+
+    }
+
+}
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from data_analyze.analyze_engine import analyze_file
 
 files = get_files_from_organization(TOKEN, "Beavers-linter")
 for file in files:
-    analyze_file(file["file"], "hashlib")
+    analyze_file(file["file"], "python", "hashlib")
