392c065e74b918c23783a643b055e9743b39be59
Adi-Shuker <adielgali57@gmail.com>
2022-12-01 15:06:36 +0200

    fix merge main

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from config import TOKEN
-from github_dir.github_handler import get_files_from_organization
+from vcs_api.github_api import get_files_from_organization
 from data_analyze.analyze_engine import analyze_file
 
 files = get_files_from_organization(TOKEN, "Beavers-linter")
