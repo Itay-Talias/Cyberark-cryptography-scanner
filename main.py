292ce0b576f3c18fdf3ae071166090b74a2c51bc
Itay.Talias <itay.talias@gmail.com>
2022-12-01 11:14:56 +0200

    final the map function

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from config import token
 from github_dir.github_handler import get_files_from_organization
 from mapping_files.mapping_reducer import mapping_files
 
+
 f = get_files_from_organization(token, "Beavers-linter")
 print(f)
-print(mapping_files(f, ["hashlib"]))
+print(mapping_files(f))
 
