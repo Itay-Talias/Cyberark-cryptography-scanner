d84e129ea6170d8895502a347202a222c42af623
Adi-Shuker <adielgali57@gmail.com>
2022-12-04 12:38:15 +0200

    convert json to object

--- backend/data_analyze/analyze_engine.py
+++ backend/data_analyze/analyze_engine.py
@@ -5,7 +5,7 @@ from vcs_api.github_api import get_context_from_file
 from data_analyze.libraries.libraries_data import libraries
 
 
-def analyze_file(file: json, language: str) -> list[object]:
+def analyze_file(file: object, language: str) -> list[object]:
     results = []
     for library in file["libraries"]:
         words_list = libraries[language][library]["words"]
@@ -13,6 +13,7 @@ def analyze_file(file: json, language: str) -> list[object]:
         split_context = context.splitlines()
         for word in words_list:
             for index, line in enumerate(split_context):
+                # analyze word
                 if word in line:
                     record = {"category": libraries[language][library]["category"],
                               "library": library,
