52a5427277460510c76bb846fb920450c304c380
Itay.Talias <itay.talias@gmail.com>
2022-12-04 14:20:01 +0200

    update extract vcs libraries ast

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from config import TOKEN
 from vcs_api.github_api_facade import GithubAPIFacade
-from extract_files.extract_by_libraries import extract_by_libraries
+from extract_files.extract_by_libraries import extract_by_libraries_ast
 
 g = GithubAPIFacade(TOKEN, "Beavers-linter")
-print(extract_by_libraries(g.get_files_from_organization(), ["hashlib"]))
+print(extract_by_libraries_ast(g.get_files_from_organization(), ["hashlib"]))
