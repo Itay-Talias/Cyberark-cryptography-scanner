685387c46aec42430016c8b0c1423b02b7aced6f
batelco <batel.cohen100@gmail.com>
2022-12-05 10:19:23 +0200

    integration engine

--- backend/extract_files/extract_by_libraries.py
+++ backend/extract_files/extract_by_libraries.py
@@ -1,5 +1,5 @@
 import ast
-from backend.extract_files.Import_finder import Import_finder
+from extract_files.Import_finder import Import_finder
 code_files_suffix = {"py"}
 def extract_by_libraries(filelist: list[object], libraries: list[str]) -> list[object]:
     map_list = list(map(lambda file: libraries_mapping(file, libraries), filelist))
--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, HTTPException, status, Request
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from fastapi.responses import JSONResponse
 import json
 from error_handler import error_handler
 from vcs_api.vcs_factory import create_vcs_connector
-from extract_files.extract_by_libraries import extract_by_libraries, extract_by_libraries_ast
+from extract_files.extract_by_libraries import extract_by_libraries_ast
+from data_analyze.analyze_engine import analyze_all_files
 app = FastAPI()
 
+PYTHON = "python"
 
 @app.post("/files", status_code=status.HTTP_201_CREATED)
 async def get_files(request: Request):
@@ -15,9 +18,12 @@ async def get_files(request: Request):
         error_handler.post_request(client_data=result)
         org = create_vcs_connector(token=result["token"], organization=result["organization"], vcs_type=result["vcs_type"])
         files = extract_by_libraries_ast(org.get_files_from_organization(), ["hashlib"])
+        return analyze_all_files(files, PYTHON)
     except ValueError as error:
-        print(error)
-    return files
+        return JSONResponse({"Error": str(error)}, status_code=status.HTTP_400_BAD_REQUEST)
+    except TypeError as error:
+        return JSONResponse({"Error": str(error)}, status_code=status.HTTP_400_BAD_REQUEST)
+
 
 
 origins = [
