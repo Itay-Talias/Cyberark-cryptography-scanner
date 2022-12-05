0c0abb87b81966f032cdd2017a6cd8e3337eddb5
Itay.Talias <itay.talias@gmail.com>
2022-12-05 15:19:54 +0200

    v1 - algo finder

--- backend/data_analyze/functions_finder.py
+++ backend/data_analyze/functions_finder.py
@@ -12,6 +12,10 @@ def find_functions(source: str, language: str, library:str):
                 and is_lib_function(node.func.attr, library, language)
                 ):
             functions_uses.append({"line-index" :node.lineno, "name" :node.func.attr})
+        elif (isinstance(node, ast.Call)
+            and isinstance(node.func, ast.Name)
+                 and is_lib_function(node.func.id, library, language)):
+                functions_uses.append({"line-index": node.lineno, "name": node.func.id})
     return  functions_uses
 def is_lib_function(func: str, library: str, language: str) -> bool:
     functions_list = libraries[language][library]["words"]
