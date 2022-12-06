c3280c8558a4a4d5ba96a8f3423fff558ff6cdf0
shalev <shalevsharf@gmail.com>
2022-12-06 15:39:25 +0200

    encrypt models

---
+++ configuration/libraries/model/crypto_usage.py
@@ -0,0 +1,25 @@
+from typing import List
+from enum import Enum
+from pydantic.dataclasses import dataclass
+
+
+class Category(Enum):
+    HASH = 1
+    MAC = 2
+    KEY_EXCHANGE = 3
+    SIGNATURE = 4
+    ENCRYPTION = 5
+
+
+@dataclass
+class CryptoUsage:
+    word: str
+    category: Category
+    algorithms: List[str]
+    params: List[object]
+
+
+@dataclass
+class Key:
+    name: str
+    length: int
