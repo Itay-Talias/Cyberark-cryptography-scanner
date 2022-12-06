67ce0fce213d24a3252329fbb0e0007143e9da01
shalev <shalevsharf@gmail.com>
2022-12-06 16:56:15 +0200

    refactor

---
+++ configuration/libraries/cryptography/libraries_data.py
@@ -0,0 +1,13 @@
+from configuration.models.crypto_usage import CryptoUsage, Category, Key
+
+libraries = {
+    "python": {
+        " cryptography": {[
+        CryptoUsage("")
+        ]
+
+        }
+    }
+
+}
+
