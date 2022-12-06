1e48aa70326f933688cc1c220c961aab0aed3883
shalev <shalevsharf@gmail.com>
2022-12-06 17:00:54 +0200

    encrypt models

--- configuration/libraries/cryptography/libraries_data.py
+++ configuration/libraries/cryptography/libraries_data.py
@@ -1,13 +1,31 @@
-from configuration.models.crypto_usage import CryptoUsage, Category, Key
+from typing import List
+from enum import Enum
+from pydantic.dataclasses import dataclass
 
-libraries = {
+class Category(Enum):
+    HASH = 1
+    MAC = 2
+    KEY_EXCHANGE = 3
+    SIGNATURE = 4
+    ENCRYPTION = 5
+
+
+@dataclass
+class CryptoUsage:
+    word: str
+    category: Category
+    algorithms: List[str]
+    params: List[object]
+
+
+@dataclass
+class Key:
+    name: str
+    length: int
+
+encrypt_libraries = {
     "python": {
-        " cryptography": {[
-        CryptoUsage("")
-        ]
-
-        }
+        " cryptography":["CryptoUsage"]
     }
-
 }
 
