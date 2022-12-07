88c49eaf4937375fc7a62e924d4f3487396f8200
Itay.Talias <itay.talias@gmail.com>
2022-12-07 11:31:44 +0200

    rename

--- backend/data_analyze/analyze_engine.py
+++ backend/data_analyze/analyze_engine.py
@@ -1,23 +1,21 @@
 from vcs_api.github_api import GithubAPI
 from data_analyze.libraries.libraries_data import libraries
-from .functions_finder import find_functions
 import ast
-from .Fun_finder import Call_finder
+from .functoins_finder import Call_finder
 
 
 def analyze_file(file: object, language: str) -> list[object]:
-    one_file_results = []
     for library in file["libraries"]:
-        context = GithubAPIFacade.get_context_from_file(file["file"])
+        context = GithubAPI.get_context_from_file(file["file"])
         tree = ast.parse(context, mode='exec')
         finder = Call_finder(language=language, library=library)
         finder.visit(tree)
         algorithm_uses = finder.functions
-        # algorithm_uses = find_functions(context, language, library)
         file_results = {
             "category": libraries[language][library]["category"],
             "library": library,
             "algorithms": algorithm_uses,
+            "utl": file["url"],
             "location": {"repo": file["repo"],
                          "path": file["file"].path}
         }
