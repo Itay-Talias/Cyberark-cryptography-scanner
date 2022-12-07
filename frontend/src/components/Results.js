46f0b0a7eddf90076d8d462d1e44c9193ad12f53
batelco <batel.cohen100@gmail.com>
2022-12-07 15:23:18 +0200

    front changes

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -2,11 +2,23 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import EnhancedTable from "./ResultsTable";
+import Box from '@mui/material/Box';
 
 export default function Results() {
     return (
         <Grid container alignItems="center" justifyContent="center">
             <Grid item>
+            <Box
+                component="img"
+                sx={{
+                  height: 200,
+                  width: 200,
+                  maxHeight: { xs: 200, md: 200 },
+                  maxWidth: { xs: 200, md: 200 },
+                }}
+                alt="cyberark logo"
+                src="https://avatars.githubusercontent.com/u/30869256?s=280&v=4"
+              />
                 <Typography variant="h3" gutterBottom>
                     CyberArk Cryptography Scanner
                 </Typography>
--- frontend/src/components/ResultsTable.js
+++ frontend/src/components/ResultsTable.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import { alpha } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -10,22 +9,16 @@ import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import TableSortLabel from "@mui/material/TableSortLabel";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
-import DeleteIcon from "@mui/icons-material/Delete";
-import FilterListIcon from "@mui/icons-material/FilterList";
 import { visuallyHidden } from "@mui/utils";
 import { ResultsContext } from "../App";
 import LinearProgress from "@mui/material/LinearProgress";
-import Grid from "@mui/material/Grid";
 
 function createData(results) {
+    console.log(results)
     const rows =
         results.length > 0
             ? results.map((arr) => {
@@ -118,7 +111,7 @@ const headCells = [
         id: "category",
         numeric: false,
         disablePadding: false,
-        label: "category",
+        label: "Category",
     },
 ];
 
@@ -137,25 +130,14 @@ function EnhancedTableHead(props) {
 
     return (
         <TableHead>
-            <TableRow>
-                <TableCell padding="checkbox">
-                    <Checkbox
-                        color="primary"
-                        indeterminate={
-                            numSelected > 0 && numSelected < rowCount
-                        }
-                        checked={rowCount > 0 && numSelected === rowCount}
-                        onChange={onSelectAllClick}
-                        inputProps={{
-                            "aria-label": "select all desserts",
-                        }}
-                    />
+            <TableRow >
+                <TableCell >
+
                 </TableCell>
                 {headCells.map((headCell) => (
                     <TableCell
                         key={headCell.id}
                         align={headCell.numeric ? "center" : "left"}
-                        // align="left"
                         padding={headCell.disablePadding ? "none" : "normal"}
                         sortDirection={orderBy === headCell.id ? order : false}
                     >
@@ -187,64 +169,6 @@ EnhancedTableHead.propTypes = {
     order: PropTypes.oneOf(["asc", "desc"]).isRequired,
     orderBy: PropTypes.string.isRequired,
     rowCount: PropTypes.number.isRequired,
-};
-
-function EnhancedTableToolbar(props) {
-    const { numSelected } = props;
-
-    return (
-        <Toolbar
-            sx={{
-                pl: { sm: 2 },
-                pr: { xs: 1, sm: 1 },
-                ...(numSelected > 0 && {
-                    bgcolor: (theme) =>
-                        alpha(
-                            theme.palette.primary.main,
-                            theme.palette.action.activatedOpacity
-                        ),
-                }),
-            }}
-        >
-            {numSelected > 0 ? (
-                <Typography
-                    sx={{ flex: "1 1 100%" }}
-                    color="inherit"
-                    variant="subtitle1"
-                    component="div"
-                >
-                    {numSelected} selected
-                </Typography>
-            ) : (
-                <Typography
-                    sx={{ flex: "1 1 100%" }}
-                    variant="h6"
-                    id="tableTitle"
-                    component="div"
-                >
-                    Results
-                </Typography>
-            )}
-
-            {numSelected > 0 ? (
-                <Tooltip title="Delete">
-                    <IconButton>
-                        <DeleteIcon />
-                    </IconButton>
-                </Tooltip>
-            ) : (
-                <Tooltip title="Filter list">
-                    <IconButton>
-                        <FilterListIcon />
-                    </IconButton>
-                </Tooltip>
-            )}
-        </Toolbar>
-    );
-}
-
-EnhancedTableToolbar.propTypes = {
-    numSelected: PropTypes.number.isRequired,
 };
 
 export default function EnhancedTable() {
@@ -313,7 +237,6 @@ export default function EnhancedTable() {
     return (
         <Box sx={{ width: "80%" }}>
             <Paper sx={{ width: "100%", mb: 2 }}>
-                <EnhancedTableToolbar numSelected={selected.length} />
                 <TableContainer>
                     <Table
                         sx={{ minWidth: 750 }}
