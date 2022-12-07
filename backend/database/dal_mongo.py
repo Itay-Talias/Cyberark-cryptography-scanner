a9856d00c06ffa0287bd610462b748ee6f2ec12b
Itay.Talias <itay.talias@gmail.com>
2022-12-07 13:29:54 +0200

    dal_mongo

---
+++ backend/database/dal_mongo.py
@@ -0,0 +1,12 @@
+class DALMongoDB:
+    def __init__(self, libraries_data: object):
+        self.libraries_data = libraries_data
+
+    def get_libraries_names(self,language: str):
+        return self.libraries_data[language].keys()
+
+    def get_functions_words_from_libraries(self,language: str,library: str):
+        return list(map(lambda l: l["word"],self.libraries_data[language][library]["words"]))
+
+    def get_function_details(self,language: str,library: str,function_name: str):
+        return list(filter(lambda l: l["word"] == function_name, self.libraries_data[language][library]["words"]))[0]
--- backend/database/sql_queries/dal_mongo.py
+++
@@ -1,7 +0,0 @@
-
-class DALmongoDB:
-    def __init__(self, libraries : object):
-        self.libraries  = libraries
-
-    def get_libraries_names(self):
-        pass
