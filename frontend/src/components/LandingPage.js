6ee96ddd0aa64f0990691e19a8661914050fcfac
Itay.Talias <itay.talias@gmail.com>
2022-12-08 17:17:13 +0200

    change landing page style

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { CryptographyScannerApi } from "../api/CryptographyScannerApi";
 import { ResultsContext } from "../App";
 import { useNavigate } from "react-router-dom";
+import "../style/LandingPage.css";
 
 export default function LandingPage() {
     const [organization, setOrganization] = useState("");
@@ -24,61 +25,70 @@ export default function LandingPage() {
     }
 
     return (
-        <Grid container alignItems="center" justifyContent="center">
-            <Grid item>
-                <Box
-                component="img"
-                sx={{
-                  height: 200,
-                  width: 200,
-                  maxHeight: { xs: 200, md: 200 },
-                  maxWidth: { xs: 200, md: 200 },
-                }}
-                alt="cyberark logo"
-                src="https://avatars.githubusercontent.com/u/30869256?s=280&v=4"
-              />
-                <Typography variant="h3" gutterBottom>
-                    CyberArk Cryptography Scanner
-                </Typography>
-            </Grid>
-            <Grid item container justifyContent="center">
-                <div>
+        <div className="container-login">
+            <Grid container alignItems="center">
+                <Grid item xs={6} className="left-box">
                     <Box
+                        component="img"
                         sx={{
-                            width: 500,
-                            maxWidth: "100%",
+                            height: 220,
+                            width: 470,
                         }}
+                        alt="cyberark logo"
+                        src="https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_a1ff161ba2b69dfe821e2c53d0e3b958/cyberark-privileged-access-manager.png
+                    "
+                    />
+                    <Typography
+                        variant="h2"
+                        gutterBottom
+                        className="title-prod"
                     >
-                        <TextField
-                            fullWidth
-                            label="organization"
-                            id="organization"
-                            margin="normal"
-                            onChange={(e) => setOrganization(e.target.value)}
-                        />
-                    </Box>
-                    <Box
-                        sx={{
-                            width: 500,
-                            maxWidth: "100%",
-                        }}
-                    >
-                        <TextField
-                            fullWidth
-                            label="token"
-                            type="password"
-                            id="token"
-                            margin="normal"
-                            onChange={(e) => setToken(e.target.value)}
-                        />
-                    </Box>
-                </div>
+                        Cryptography Scanner
+                    </Typography>
+                </Grid>
+                <Grid item justifyContent="center" xs={6} className="right-box">
+                    <div className="login-box">
+                        <Box
+                            sx={{
+                                width: 500,
+                                maxWidth: "100%",
+                            }}
+                        >
+                            <TextField
+                                fullWidth
+                                label="organization"
+                                id="organization"
+                                margin="normal"
+                                onChange={(e) =>
+                                    setOrganization(e.target.value)
+                                }
+                            />
+                        </Box>
+                        <Box
+                            sx={{
+                                width: 500,
+                                maxWidth: "100%",
+                            }}
+                        >
+                            <TextField
+                                fullWidth
+                                label="token"
+                                type="password"
+                                id="token"
+                                margin="normal"
+                                onChange={(e) => setToken(e.target.value)}
+                            />
+                        </Box>
+                        <Button
+                            className="btn-login"
+                            variant="contained"
+                            onClick={() => handleSubmit()}
+                        >
+                            Scan
+                        </Button>
+                    </div>
+                </Grid>
             </Grid>
-            <Grid item>
-                <Button variant="contained" onClick={() => handleSubmit()}>
-                    Scan
-                </Button>
-            </Grid>
-        </Grid>
+        </div>
     );
 }
--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -2,25 +2,25 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import EnhancedTable from "./ResultsTable";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
+import "../style/Results.css";
 
 export default function Results() {
     return (
         <Grid container alignItems="center" justifyContent="center">
             <Grid item>
-            <Box
-                component="img"
-                sx={{
-                  height: 200,
-                  width: 200,
-                  maxHeight: { xs: 200, md: 200 },
-                  maxWidth: { xs: 200, md: 200 },
-                }}
-                alt="cyberark logo"
-                src="https://avatars.githubusercontent.com/u/30869256?s=280&v=4"
-              />
-                <Typography variant="h3" gutterBottom>
-                    CyberArk Cryptography Scanner
+                <Box
+                    component="img"
+                    sx={{
+                        height: 100,
+                        width: 200,
+                    }}
+                    alt="cyberark logo"
+                    src="https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_a1ff161ba2b69dfe821e2c53d0e3b958/cyberark-privileged-access-manager.png
+                    "
+                />
+                <Typography variant="h3" gutterBottom className="title-result">
+                    Cryptography Scanner
                 </Typography>
             </Grid>
             <EnhancedTable></EnhancedTable>
